2dcf496d7f0e7dec3ca1487ebf64dbff43ef03ef
flip555 <109870462+flip555@users.noreply.github.com>
2023-09-10 02:02:01 +0100

    Fix

--- custom_components/seplos_bms/sensor.py
+++ custom_components/seplos_bms/sensor.py
@@ -1,25 +1,30 @@
+import logging
+import json
+from datetime import timedelta
+from homeassistant.helpers.entity import Entity
+from .seplos_helper import SeplosHelper
+from .const import DOMAIN
+from .seplos_helper import BMSDataCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+
 class SeplosBMSSensor(Entity):
-    def __init__(self, name, data_func, attr, unit=None):
+    def __init__(self, name, data_coordinator, attr, unit=None):
         self._name = name
-        self._data_func = data_func
+        self._data_coordinator = data_coordinator
         self._attr = attr
         self._unit = unit
         self._latest_data = {}
+
+    async def async_update(self):
+        await self._data_coordinator.fetch_data(self.hass)
+        self._latest_data = self._data_coordinator.data
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
         return self._unit
-
-    async def async_update_data(self):
-        try:
-            result = await self._data_func()
-            if isinstance(result, str):
-                self._latest_data = json.loads(result)
-            else:
-                self._latest_data = result
-        except Exception as e:
-            _LOGGER.error("Error fetching data: %s", e)
 
     @property
     def name(self):
@@ -28,6 +33,9 @@ class SeplosBMSSensor(Entity):
     @property
     def state(self):
         data = self._latest_data
+        if isinstance(data, str):
+            data = json.loads(data)
+        _LOGGER.debug("Dat: %s", data)
         for attr_part in self._attr.split('.'):
             if isinstance(data, dict) and attr_part in data:
                 data = data[attr_part]
@@ -35,36 +43,36 @@ class SeplosBMSSensor(Entity):
                 return None
         return data
 
+        
+
 async def async_setup_entry(hass, entry, async_add_entities):
     usb_port = entry.data["usb_port"]
-    async def get_data():
-        return await hass.async_add_executor_job(fetch_data_from_bms, usb_port)
+    data_coordinator = BMSDataCoordinator(usb_port)
 
     sensors = [
-        SeplosBMSSensor("Seplos BMS - Number of Cells", get_data, "NoCells"),
-        SeplosBMSSensor("Seplos BMS - Highest Cell Value", get_data, "HighestCell", "V"),
-        SeplosBMSSensor("Seplos BMS - Lowest Cell Value", get_data, "LowestCell", "V"),
-        SeplosBMSSensor("Seplos BMS - Cell Difference", get_data, "CellDifference", "V"),
-        SeplosBMSSensor("Seplos BMS - Environment Temperature", get_data, "EnvTemp", "°C"),
-        SeplosBMSSensor("Seplos BMS - Power Temperature", get_data, "PowerTemp", "°C"),
-        SeplosBMSSensor("Seplos BMS - Current", get_data, "Current", "A"),
-        SeplosBMSSensor("Seplos BMS - Voltage", get_data, "Voltage", "V"),
-        SeplosBMSSensor("Seplos BMS - Capacity Remaining", get_data, "CapRemain", "Ah"),
-        SeplosBMSSensor("Seplos BMS - Capacity in Wh Remaining", get_data, "CapwHRemain", "Wh"),
-        SeplosBMSSensor("Seplos BMS - Total Capacity", get_data, "Cap", "Ah"),
-        SeplosBMSSensor("Seplos BMS - State of Charge (SOC)", get_data, "SOC", "%"),
-        SeplosBMSSensor("Seplos BMS - Total Capacity", get_data, "Capacity", "Ah"),
-        SeplosBMSSensor("Seplos BMS - Number of Cycles", get_data, "Cycles"),
-        SeplosBMSSensor("Seplos BMS - State of Health (SOH)", get_data, "SOH", "%"),
-        SeplosBMSSensor("Seplos BMS - Port Voltage", get_data, "PortV", "V"),
-        SeplosBMSSensor("Seplos BMS - Temperature 1", get_data, "Temp1", "°C"),
-        SeplosBMSSensor("Seplos BMS - Temperature 2", get_data, "Temp2", "°C"),
-        SeplosBMSSensor("Seplos BMS - Temperature 3", get_data, "Temp3", "°C"),
-        SeplosBMSSensor("Seplos BMS - Temperature 4", get_data, "Temp4", "°C"),
-    ]
+        SeplosBMSSensor("Seplos BMS - Number of Cells", data_coordinator, "NoCells"),
+        SeplosBMSSensor("Seplos BMS - Highest Cell Value", data_coordinator, "HighestCell", "V"),
+        SeplosBMSSensor("Seplos BMS - Lowest Cell Value", data_coordinator, "LowestCell", "V"),
+        SeplosBMSSensor("Seplos BMS - Cell Difference", data_coordinator, "CellDifference", "V"),
+        SeplosBMSSensor("Seplos BMS - Environment Temperature", data_coordinator, "EnvTemp", "°C"),
+        SeplosBMSSensor("Seplos BMS - Power Temperature", data_coordinator, "PowerTemp", "°C"),
+        SeplosBMSSensor("Seplos BMS - Current", data_coordinator, "Current", "A"),
+        SeplosBMSSensor("Seplos BMS - Voltage", data_coordinator, "Voltage", "V"),
+        SeplosBMSSensor("Seplos BMS - Capacity Remaining", data_coordinator, "CapRemain", "Ah"),
+        SeplosBMSSensor("Seplos BMS - Capacity in Wh Remaining", data_coordinator, "CapwHRemain", "Wh"),
+        SeplosBMSSensor("Seplos BMS - Total Capacity", data_coordinator, "Cap", "Ah"),
+        SeplosBMSSensor("Seplos BMS - State of Charge (SOC)", data_coordinator, "SOC", "%"),
+        SeplosBMSSensor("Seplos BMS - Total Capacity", data_coordinator, "Capacity", "Ah"),
+        SeplosBMSSensor("Seplos BMS - Number of Cycles", data_coordinator, "Cycles"),
+        SeplosBMSSensor("Seplos BMS - State of Health (SOH)", data_coordinator, "SOH", "%"),
+        SeplosBMSSensor("Seplos BMS - Port Voltage", data_coordinator, "PortV", "V"),
+        SeplosBMSSensor("Seplos BMS - Temperature 1", data_coordinator, "Temp1", "°C"),
+        SeplosBMSSensor("Seplos BMS - Temperature 2", data_coordinator, "Temp2", "°C"),
+        SeplosBMSSensor("Seplos BMS - Temperature 3", data_coordinator, "Temp3", "°C"),
+        SeplosBMSSensor("Seplos BMS - Temperature 4", data_coordinator, "Temp4", "°C"),    ]
 
     # Dynamically add cell sensors
     for i in range(1, 17):  # 16 cells
-        sensors.append(SeplosBMSSensor(f"Seplos BMS - Cell {i} Voltage", get_data, f"C{i}", "V"))
+        sensors.append(SeplosBMSSensor(f"Seplos BMS - Cell {i} Voltage", data_coordinator, f"C{i}", "V"))
 
     async_add_entities(sensors, True)
--- custom_components/seplos_bms/seplos_helper.py
+++ custom_components/seplos_bms/seplos_helper.py
@@ -1,81 +1,120 @@
 import time
 import serial
 import json
+import logging
+_LOGGER = logging.getLogger(__name__)
 
-def twos_complement(value, bits=16):
-    if (value & (1 << (bits - 1))):
-        value -= 1 << bits
-    return value
+import asyncio
 
-def get_div(rdata, offset, divisor, precision=2, signed=False):
-    N = int(rdata[offset:offset+4], 16)
-    offset += 4
-    if signed:
-        N = twos_complement(N)
-    return round(N / divisor, precision), offset
+class BMSDataCoordinator:
+    _lock = asyncio.Lock()
 
-def fetch_data_from_bms(usb_port):
-    retries = 0
-    max_retries = 3
-    while retries < max_retries:
-        data = {}
-        offset = 19
-        with serial.Serial(usb_port, 19200, timeout=1) as ser:
-            ser.write(COMMAND.encode())
-            response = ser.read_until().decode()
+    def __init__(self, usb_port):
+        self.usb_port = usb_port
+        self.data = {}
+        self.last_fetch = None
 
-        rdata = response
-        if rdata[7:9] != "00":
-            print(f"Error code {rdata[7:9]}. Retrying...")
-            retries += 1
-            time.sleep(0.5)
-            continue
+    async def fetch_data(self, hass):
+        async with BMSDataCoordinator._lock:  # Prevent simultaneous fetches
+            if self.last_fetch is None or time.time() - self.last_fetch > 10:  # Fetch every 10 seconds
+                _LOGGER.debug("FETCH_DATA TRIGGERED")
+                self.data = await hass.async_add_executor_job(SeplosHelper.fetch_data_from_bms, self.usb_port)
+                self.last_fetch = time.time()
 
-        # Extract NCELL and related data
-        NCELL = int(rdata[17:19], 16)
-        data['NoCells'] = NCELL
-        highestValue = float('-inf')
-        lowestValue = float('inf')
-        for l in range(NCELL):
-            V = int(rdata[offset:offset+4], 16)
-            offset += 4
-            if V > highestValue:
-                highestValue = V
-            if V < lowestValue:
-                lowestValue = V
-            data[f"C{l+1}"] = V
 
-        data["HighestCell"] = highestValue
-        data["LowestCell"] = lowestValue
-        data["CellDifference"] = highestValue - lowestValue
+class SeplosHelper:
+    COMMAND = "~20004642E00201FD36\r"
 
-        # Extract NTEMPS and related data
-        NTEMPS = int(rdata[offset:offset+2], 16)
-        offset += 2
-        for l in range(1, NTEMPS + 1):
-            T = int(rdata[offset:offset+4], 16)
-            offset += 4
-            T = round((T - 2731) / 10, 1)
-            if l == NTEMPS - 1:
-                data["EnvTemp"] = T
-            elif l == NTEMPS:
-                data["PowerTemp"] = T
-            else:
-                data[f"Temp{l}"] = T
+    @staticmethod
+    def twos_complement(value, bits=16):
+        if (value & (1 << (bits - 1))):
+            value -= 1 << bits
+        return value
 
-        # Parse remaining parameters
-        data['Current'], offset = get_div(rdata, offset, 100, 2, True)
-        data['Voltage'], offset = get_div(rdata, offset, 100)
-        data['CapRemain'], offset = get_div(rdata, offset, 100)
-        data['CapwHRemain'] = data['CapRemain'] * 48
-        offset += 2
-        data['Cap'], offset = get_div(rdata, offset, 100)
-        data['SOC'], offset = get_div(rdata, offset, 10, 1)
-        data['Capacity'], offset = get_div(rdata, offset, 100)
-        data['Cycles'], offset = get_div(rdata, offset, 1, 0)
-        data['SOH'], offset = get_div(rdata, offset, 10, 1)
-        data['PortV'], offset = get_div(rdata, offset, 100, 2)
+    @staticmethod
+    def get_div(rdata, offset, divisor, precision=2, signed=False):
+        N = int(rdata[offset:offset+4], 16)
+        offset += 4
+        if signed:
+            N = SeplosHelper.twos_complement(N)
+        return round(N / divisor, precision), offset
 
-        return json.dumps(data)
+    @staticmethod
+    def fetch_data_from_bms(usb_port):
+        retries = 0
+        max_retries = 3
+        while retries < max_retries:
+            data = {}
+            offset = 19
+            with serial.Serial(usb_port, 19200, timeout=1) as ser:
+                _LOGGER.debug("Writing to Serial from: %s", SeplosHelper.COMMAND)
+                ser.write(SeplosHelper.COMMAND.encode())
+                try:
+                    ser.timeout = 5
+                    response = ser.read_until().decode()
 
-    raise Exception("Failed to fetch data after maximum retries.")
+                except (serial.SerialTimeoutException, serial.SerialException) as e:
+                    _LOGGER.error(f"Error reading from serial port: {e}")
+                    return None
+
+                _LOGGER.debug("Response from: %s", response)
+
+            rdata = response
+            if rdata[7:9] != "00":
+                print(f"Error code {rdata[7:9]}. Retrying...")
+                retries += 1
+                time.sleep(0.5)
+                continue
+
+            # Extract NCELL and related data
+            NCELL = int(rdata[17:19], 16)
+            data['NoCells'] = NCELL
+            highestValue = float('-inf')
+            lowestValue = float('inf')
+            for l in range(NCELL):
+                V = int(rdata[offset:offset+4], 16)
+                offset += 4
+                if V > highestValue:
+                    highestValue = V
+                if V < lowestValue:
+                    lowestValue = V
+                data[f"C{l+1}"] = V
+
+            data["HighestCell"] = highestValue
+            data["LowestCell"] = lowestValue
+            data["CellDifference"] = highestValue - lowestValue
+
+            # Extract NTEMPS and related data
+            NTEMPS = int(rdata[offset:offset+2], 16)
+            offset += 2
+            for l in range(1, NTEMPS + 1):
+                T = int(rdata[offset:offset+4], 16)
+                offset += 4
+                T = round((T - 2731) / 10, 1)
+                if l == NTEMPS - 1:
+                    data["EnvTemp"] = T
+                elif l == NTEMPS:
+                    data["PowerTemp"] = T
+                else:
+                    data[f"Temp{l}"] = T
+
+
+            
+            # Parse remaining parameters
+            data['Current'], offset = SeplosHelper.get_div(rdata, offset, 100, 2, True)
+            data['Voltage'], offset = SeplosHelper.get_div(rdata, offset, 100)
+            data['CapRemain'], offset = SeplosHelper.get_div(rdata, offset, 100)
+            data['CapwHRemain'] = data['CapRemain'] * 48
+            offset += 2
+            data['Cap'], offset = SeplosHelper.get_div(rdata, offset, 100)
+            data['SOC'], offset = SeplosHelper.get_div(rdata, offset, 10, 1)
+            data['Capacity'], offset = SeplosHelper.get_div(rdata, offset, 100)
+            data['Cycles'], offset = SeplosHelper.get_div(rdata, offset, 1, 0)
+            data['SOH'], offset = SeplosHelper.get_div(rdata, offset, 10, 1)
+            data['PortV'], offset = SeplosHelper.get_div(rdata, offset, 100, 2)
+
+            _LOGGER.debug("Raw data helper %s", data)
+
+            return json.dumps(data)
+
+        raise Exception("Failed to fetch data after maximum retries.")
